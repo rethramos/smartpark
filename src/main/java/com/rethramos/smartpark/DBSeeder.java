796bd156ce0a74b33ff6631cd3a4a604041f49e6
Rethaniel Ramos <rethanielramos@gmail.com>
2026-06-03 19:12:07 +0800

    Seed vehicle types

--- src/main/java/com/rethramos/smartpark/DBSeeder.java
+++ src/main/java/com/rethramos/smartpark/DBSeeder.java
@@ -1,13 +1,18 @@
 package com.rethramos.smartpark;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 @Component
-public class DBSeeder implements CommandLineRunner  {
+public class DBSeeder implements CommandLineRunner {
     @Autowired
     private AddressRepository addressRepository;
+
+    @Autowired
+    private VehicleTypeRepository vehicleTypeRepository;
 
     @Override
     public void run(String... args) throws Exception {
@@ -16,5 +21,16 @@ public class DBSeeder implements CommandLineRunner  {
         a.setProvince("Laguna");
         a.setCountry("PH");
         addressRepository.save(a);
+
+        VehicleType car = new VehicleType();
+        car.setCode("CAR");
+        car.setName("Car");
+        VehicleType motor = new VehicleType();
+        motor.setCode("MOTOR");
+        motor.setName("Motor");
+        VehicleType truck = new VehicleType();
+        truck.setCode("TRUCK");
+        truck.setName("Truck");
+        vehicleTypeRepository.saveAll(List.of(car, truck, motor));
     }
-}    
+}
