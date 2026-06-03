ca9e0ace9791364afa8619f40f4633134b0d5562
Rethaniel Ramos <rethanielramos@gmail.com>
2026-06-03 19:17:29 +0800

    scaffold Person model

--- src/main/java/com/rethramos/smartpark/ParkingLotDto.java
+++
@@ -1,5 +0,0 @@
-package com.rethramos.smartpark;
-
-public record ParkingLotDto(String lotId, Long locationId, Integer capacity, Integer occupiedSpaces) {
-
-}
---
+++ src/main/java/com/rethramos/smartpark/Person.java
@@ -0,0 +1,49 @@
+package com.rethramos.smartpark;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String firstName;
+    private String middleName;
+    private String lastName;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+}
