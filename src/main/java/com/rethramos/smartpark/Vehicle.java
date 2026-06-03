c4eadddfdd084b56aff02a072aa382ceb8d4b948
Rethaniel Ramos <rethanielramos@gmail.com>
2026-06-03 19:05:21 +0800

    scaffold Vehicle models

---
+++ src/main/java/com/rethramos/smartpark/Vehicle.java
@@ -0,0 +1,54 @@
+package com.rethramos.smartpark;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
+
+@Entity
+public class Vehicle {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String licensePlate;
+    @OneToOne(optional = false)
+    private VehicleType vehicleType;
+    // Person owner;
+    @ManyToOne
+    private ParkingLot parking;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getLicensePlate() {
+        return licensePlate;
+    }
+
+    public void setLicensePlate(String licensePlate) {
+        this.licensePlate = licensePlate;
+    }
+
+    public VehicleType getVehicleType() {
+        return vehicleType;
+    }
+
+    public void setVehicleType(VehicleType vehicleType) {
+        this.vehicleType = vehicleType;
+    }
+
+    public ParkingLot getParking() {
+        return parking;
+    }
+
+    public void setParking(ParkingLot parking) {
+        this.parking = parking;
+    }
+
+}
---
+++ src/main/java/com/rethramos/smartpark/VehicleTypeRepository.java
@@ -0,0 +1,7 @@
+package com.rethramos.smartpark;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VehicleTypeRepository extends JpaRepository<VehicleType, Long> {
+
+}
