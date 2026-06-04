b00af19c3c104150278aabd1ea98a40c757c45d3
Rethaniel Ramos <rethanielramos@gmail.com>
2026-06-04 19:45:29 +0800

    add generic db constraint exception handler

--- src/main/java/com/rethramos/smartpark/RestExceptionHandler.java
+++ src/main/java/com/rethramos/smartpark/RestExceptionHandler.java
@@ -2,6 +2,7 @@ package com.rethramos.smartpark;
 
 import java.util.NoSuchElementException;
 
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -11,5 +12,10 @@ public class RestExceptionHandler {
     @ExceptionHandler(NoSuchElementException.class)
     public ResponseEntity<Void> handleNoSuchElement(NoSuchElementException e) {
         return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<Void> handleDBConstraintViolation(DataIntegrityViolationException e) {
+        return ResponseEntity.badRequest().build();
     }
 }
